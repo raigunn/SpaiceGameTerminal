9ded63679b158cbb530623677e21d7f49a9bfbe8
Daniel DeLay <daniel@velir.com>
2016-05-02 08:59:05 -0400

    added ascii display

---
+++ ConsoleRestTerminal/Models/Responses/PingResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConsoleRestTerminal.Models.Responses
+{
+	[DataContract]
+	public class PingResponse
+	{
+		[DataMember(Name = "ping")]
+		public string Ping { get; set; }
+	}
+}
+
--- ConsoleRestTerminal/Program.cs
+++ ConsoleRestTerminal/Program.cs
@@ -3,27 +3,35 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.Serialization.Formatters;
 using System.Runtime.Serialization.Json;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
+using ConsoleRestTerminal.Models.Requests;
+using ConsoleRestTerminal.Models.Responses;
 
 namespace ConsoleRestTerminal
 {
 	//string url = "http://localhost:8000/DEMOService/";
+
 	class Program
 	{
+		private static string _url = "";
 		static void Main(string[] args)
 		{
 			// begin dialog to get url of server
-			string url = GetUrl();
-			if (url.Equals("exit")) return;
+			_url = GetUrl();
+			if (_url.Equals("exit")) return;
 
-			Console.WriteLine("sending gamestate");
-			GameState gameState = MakeGameState();
-			TurnResponse turnResponse = MakePostRequest<GameState, TurnResponse>(url + "Turn", gameState);
-			ProcessTurnResponse(turnResponse);
-			
+			string result = GetTurnCount();
+			if (result.Equals("exit")) return;
+
+
+			Thread.Sleep(1500);
+			DrawGame();
+
 			// exit on keystroke
 			Console.ReadKey();
 		}
@@ -38,6 +46,7 @@ namespace ConsoleRestTerminal
 			{
 				url = Console.ReadLine();
 				if (url == "exit") return "exit";
+				if (url == "default1") url = "http://localhost:8000/DEMOService/";
 
 				Console.WriteLine("");
 				PingResponse pingResponse = MakeGetRequest<PingResponse>(url + "Json");
@@ -55,6 +64,51 @@ namespace ConsoleRestTerminal
 		}
 
 
+		private static string GetTurnCount()
+		{
+			Console.WriteLine("Enter number of rounds to begin game, or type 'exit'");
+
+			string turnCount = "";
+			int intTurnCount;
+			bool isValid = false;
+			do
+			{
+				turnCount = Console.ReadLine();
+				if (turnCount == "exit"  ) return "exit";
+				if (!int.TryParse(turnCount, out intTurnCount) || intTurnCount < 1 || intTurnCount > 100)
+				{
+					Console.WriteLine("Please enter a valid number");
+				}
+				else
+				{
+					TurnResponse turnResponse = new TurnResponse();
+					int x = 0;
+					int y = 1;
+					Console.WriteLine("Starting Game...");
+					for (int i = 0; i < intTurnCount; i++)
+					{
+						GameState gameState;
+						if (i == 0)
+						{
+							gameState = MakeInitialGameState();
+						}
+						else
+						{
+							if (turnResponse.MoveDirection == 1) x = x + 1;
+							if (turnResponse.MoveDirection == 3) x = x - 1;
+							if (turnResponse.MoveDirection == 0) y = y - 1;
+							if (turnResponse.MoveDirection == 2) y = y + 1;
+							gameState = MakeGameState(i + 1, x, y);
+						}
+
+						turnResponse = MakePostRequest<GameState, TurnResponse>(_url + "Turn", gameState);
+						ProcessTurnResponse(turnResponse);
+					}
+					isValid = true;
+				}
+			} while (!isValid);
+			return "done";
+		}
 
 
 
@@ -88,17 +142,27 @@ namespace ConsoleRestTerminal
 		}
 
 
-		private static GameState MakeGameState()
+		private static GameState MakeInitialGameState()
 		{
 			var gameState = new GameState()
 			{
-				Round = 5,
+				Round = 1,
 				GridSize = new int[] {8,8},
 				Players = new List<int[]>() { new int[] {0, 1}, new int[] {2, 3}}
 			};
 			return gameState;
 		}
 
+		private static GameState MakeGameState(int round, int x, int y)
+		{
+			var gameState = new GameState()
+			{
+				Round = round,
+				GridSize = new int[] { 8, 8 },
+				Players = new List<int[]>() { new int[] { x, y }, new int[] { 2, 3 } }
+			};
+			return gameState;
+		}
 
 
 
@@ -148,6 +212,100 @@ namespace ConsoleRestTerminal
 			}
 		}
 
+
+		// if x == 0; replace [1-4]
+		// if x == 1; replace [6-9]
+		// if x == 2; replace [11-14]
+		// if y == 0; replace [1-2]
+		// if y == 1; replace [4-5]
+		// if y == 2; replace [7-8]
+		// initial x offset == 1
+		//	then add x * 5
+		// initial y offset == 1
+		// then add y * 3
+		private static void DrawGame()
+		{
+			Console.CursorVisible = false;
+
+			string shipTop =		@"/''\";
+			string shipBottom = @"\__/";
+
+			var arr = new[]
+			{
+				@"+----+----+----+----+----+----+----+----+",
+				@"|    |    |    |    |    |    |    |    |",
+				@"|    |    |    |    |    |    |    |    |",
+				@"+----+----+----+----+----+----+----+----+",
+				@"|    |    |    |    |    |    |    |    |",
+				@"|    |    |    |    |    |    |    |    |",
+				@"+----+----+----+----+----+----+----+----+",
+				@"|    |    |    |    |    |    |    |    |",
+				@"|    |    |    |    |    |    |    |    |",
+				@"+----+----+----+----+----+----+----+----+",
+				@"|    |    |    |    |    |    |    |    |",
+				@"|    |    |    |    |    |    |    |    |",
+				@"+----+----+----+----+----+----+----+----+",
+				@"|    |    |    |    |    |    |    |    |",
+				@"|    |    |    |    |    |    |    |    |",
+				@"+----+----+----+----+----+----+----+----+",
+				@"|    |    |    |    |    |    |    |    |",
+				@"|    |    |    |    |    |    |    |    |",
+				@"+----+----+----+----+----+----+----+----+",
+				@"|    |    |    |    |    |    |    |    |",
+				@"|    |    |    |    |    |    |    |    |",
+				@"+----+----+----+----+----+----+----+----+",
+				@"|    |    |    |    |    |    |    |    |",
+				@"|    |    |    |    |    |    |    |    |",
+				@"+----+----+----+----+----+----+----+----+"
+			};
+
+
+			Console.WindowWidth = 100;
+			Console.WindowHeight = 40;
+			Console.Clear();
+			foreach (string line in arr)
+			{
+				Console.WriteLine(line);
+			}
+			Thread.Sleep(500);
+
+
+
+			List<int[]> testPositions = new List<int[]>();
+			testPositions.Add(new[] { 0, 0 });
+			testPositions.Add(new[] { 1, 1 });
+			testPositions.Add(new[] { 2, 2 });
+			testPositions.Add(new[] { 3, 3 });
+			testPositions.Add(new[] { 4, 4 });
+
+			foreach (var testPosition in testPositions)
+			{
+				int rowIndexA = 1 + (testPosition[1]*3);
+				int rowIndexB = rowIndexA + 1;
+				string rowA = arr[rowIndexA];
+				string rowB = arr[rowIndexB];
+				int colIndexA = 1 + (testPosition[0]*5);
+				int colIndexB = colIndexA + 4;
+				StringBuilder sbA = new StringBuilder(rowA);
+				sbA.Replace("    ", shipTop, colIndexA, 4);
+				StringBuilder sbB = new StringBuilder(rowB);
+				sbB.Replace("    ", shipBottom, colIndexA, 4);
+				arr[rowIndexA] = sbA.ToString();
+				arr[rowIndexB] = sbB.ToString();
+
+
+				Console.Clear();
+				foreach (string line in arr)
+				{
+					Console.WriteLine(line);
+				}
+				Thread.Sleep(500);
+			}
+
+
+
+			Console.ReadKey();
+		}
 
 	}
 }
